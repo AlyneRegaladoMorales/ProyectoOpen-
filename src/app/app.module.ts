34d8d4d1463e8cfc9079018d8330d73d2933e7d9
Alyne Nayely Regalado Morales <alyn.remo@gmail.com>
2023-06-04 18:34:56 -0500

    Creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IniciarSesionComponent } from './components/registro/iniciar-sesion/iniciar-sesion.component';
+import { RegistrarUsuarioComponent } from './components/registro/registrar-usuario/registrar-usuario.component';
+import { CambiarContraseniaComponent } from './components/registro/cambiar-contrasenia/cambiar-contrasenia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IniciarSesionComponent,
+    RegistrarUsuarioComponent,
+    CambiarContraseniaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
