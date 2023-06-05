a667d74efaa116597f553a6f9b10f5e28c114d86
Alyne Nayely Regalado Morales <alyn.remo@gmail.com>
2023-06-04 21:05:48 -0500

    Interfaz de Registro de usuario cliente y emprendedor

--- src/app/components/registro/registrar-usuario/registrar-usuario.component.ts
+++ src/app/components/registro/registrar-usuario/registrar-usuario.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ServidorService } from 'src/app/services/servidor.service';
 
 @Component({
   selector: 'app-registrar-usuario',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./registrar-usuario.component.css']
 })
 export class RegistrarUsuarioComponent {
+constructor(
+    private builder: FormBuilder,
+    private servicio: ServidorService,
+    private router: Router) { }
 
+  usuario = this.builder.group({
+    nombre: this.builder.control('', Validators.required),
+    apellido: this.builder.control('', Validators.required),
+    celular: this.builder.control('', Validators.required),
+    email: this.builder.control('', Validators.compose([Validators.required, Validators.email])),
+
+    id: this.builder.control('', Validators.required),
+    password: this.builder.control('', Validators.required),
+    rol: this.builder.control('Cliente'),
+
+  })
+
+    registrarUsuario() {
+    
+  }
 }
